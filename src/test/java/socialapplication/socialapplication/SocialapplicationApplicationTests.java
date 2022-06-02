1e75e1f6946a8b3a6926818b36238e312c0cb34d
Fevenf <101144416+Fevenf@users.noreply.github.com>
2022-06-02 15:32:36 -0400

    First commit

---
+++ src/test/java/socialapplication/socialapplication/SocialapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package socialapplication.socialapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
